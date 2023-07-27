79bb73dd08f08320dd7d77a744eaa573e25a7212
alexis <alexissimoes94@gmail.com>
2023-07-27 10:54:45 +0200

    alexis_v1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <img src="img/silica_help.png" alt="" id="help">
+    <div class="sakura"></div>
+  <script src="js/structure.js"></script>
+    <div class="form">
+        <form action="form">
+            <label for="nam">First nam</label>
+            <input id="nam" type="text" placeholder="First nam"> <br>
+
+            <label for="nam2">last nam</label>
+            <input id="nam2" type="text" placeholder="last nam"> <br>
+
+            <label for="mail">E-mail</label>
+            <input id="mail" type="text" placeholder="First nam"> <br>
+
+            <input type="submit" id="submit">
+           <button id="google"><img class="img1" src="img/google.png" alt="">   sincrire avec google</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ js/structure.js
@@ -0,0 +1,31 @@
+function createSakura() {
+  const sakura = document.createElement('div');
+  sakura.classList.add('sakura');
+  document.body.appendChild(sakura);
+
+  const startX = Math.random() * window.innerWidth;
+  const delay = Math.random() * 5;
+
+  sakura.style.left = startX + 'px';
+  sakura.style.animationDuration = Math.random() * 4 + 5 + 's';
+  sakura.style.animationDelay = delay + 's';
+
+  sakura.addEventListener('animationiteration', () => {
+    sakura.style.left = Math.random() * window.innerWidth + 'px';
+  });
+}
+
+for (let i = 0; i < 30; i++) {
+  createSakura();
+}
+
+
+setTimeout(function() {
+        $('#help').animate({
+          'right': '0px' 
+        }, 3000);
+      }, 1000); 
+
+setTimeout(function() {
+    console.log("ready");
+      }, 4000); 
